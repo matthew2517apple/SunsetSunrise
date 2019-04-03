96f3858ad0a035b33534744199cb5d8d9504d6f4
matthew <matthew@matthew.com>
2019-04-03 14:10:26 -0500

    Initial Commit

---
+++ SunsetSunriseTests/SunsetSunriseTests.swift
@@ -0,0 +1,34 @@
+//
+//  SunsetSunriseTests.swift
+//  SunsetSunriseTests
+//
+//  Created by MJC-iCloud on 4/3/19.
+//  Copyright © 2019 Matthew. All rights reserved.
+//
+
+import XCTest
+@testable import SunsetSunrise
+
+class SunsetSunriseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
